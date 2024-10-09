5a4cbb2bbdda9435f2df719470eda95c8c5656de
lrcarey222 <lcarey@rmi.org>
2024-10-09 10:33:51 -0400

    10/9/24

--- Econ Dev Scripts/Energy Policy Simulator.R
+++ Econ Dev Scripts/Energy Policy Simulator.R
@@ -2,6 +2,7 @@
 
 #Load Master Libraries
 eps_bau_master<-read.csv("OneDrive - RMI/Documents - US Program/6_Projects/Clean Regional Economic Development/ACRE/Data/Raw Data/eps_bau_master.csv")
+eps_ndc_master<-read.csv("OneDrive - RMI/Documents - US Program/6_Projects/Clean Regional Economic Development/ACRE/Data/Raw Data/eps_ndc_master.csv")
 
 
 #Clean Files
@@ -18,7 +19,26 @@ eps_bau <- eps_bau_master %>%
          var3 = trimws(var3)) %>%
   pivot_longer(cols=X2021:X2050,names_to="Year",values_to="Value") %>%
   mutate(Year=as.numeric(gsub("X","",Year)),
-         Value=as.numeric(Value)) %>%
+         Value=as.numeric(Value),
+         scenario="BAU") %>%
+  left_join(census_divisions,by=c("state"="State.Code")) %>%
+  select(Region,Division,State,state,var1,var2,var3,var4,Year,Value)
+
+eps_ndc <- eps_ndc_master %>%
+  separate(Time, into = c("var1", "rest"), sep = "\\[", remove = FALSE) %>%
+  separate(rest, into = c("var2", "var3"), sep = ",", fill = "right") %>%
+  separate(var3, into = c("var3", "var4"), sep = ",", fill = "right") %>%
+  mutate(var2=gsub("\\d{2}T\\d{2}", "", var2),
+         var2 = gsub("\\d|\\]", "", var2),
+         var2 = gsub("\\bes\\b", "", var2),
+         var2=trimws(var2),
+         var3 = gsub("\\]", "", var3),
+         var3=gsub("\\bif\\b", "", var3),        # Remove standalone word "if" (surrounded by spaces or end of string)
+         var3 = trimws(var3)) %>%
+  pivot_longer(cols=X2021:X2050,names_to="Year",values_to="Value") %>%
+  mutate(Year=as.numeric(gsub("X","",Year)),
+         Value=as.numeric(Value),
+         scenario="NDC") %>%
   left_join(census_divisions,by=c("state"="State.Code")) %>%
   select(Region,Division,State,state,var1,var2,var3,var4,Year,Value)
 
